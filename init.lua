933f40c6309c3abe2b6287970bb0710082d455e1
kociak1984 <ssmr.quarc+github@gmail.com>
2015-09-27 19:59:28 +0200

    Initial release. Everithing works, more signs to add...

---
+++ init.lua
@@ -0,0 +1,361 @@
+minetest.register_node("industrial_signs:biohazard", {
+	description = "Industrial Signs: Biohazard",
+	drawtype = "signlike",
+	walkable = false,
+	tiles = {"industrial_signs_biohazard.png"},
+	wield_image =  "industrial_signs_biohazard.png",
+	inventory_image =  "industrial_signs_biohazard.png",
+	paramtype = "light",
+	paramtype2 = "wallmounted",
+	selection_box = {
+		type = "wallmounted",
+	},
+	groups = {oddly_breakable_by_hand = 3}, 
+});
+
+minetest.register_node("industrial_signs:chemical_warning_1", {
+	description = "Industrial Signs: Chemical Warning 1",
+	drawtype = "signlike",
+	walkable = false,
+	tiles = {"industrial_signs_chemical_warning1.png"},
+	wield_image =  "industrial_signs_chemical_warning1.png",
+	inventory_image =  "industrial_signs_chemical_warning1.png",
+	paramtype = "light",
+	paramtype2 = "wallmounted",
+	selection_box = {
+		type = "wallmounted",
+	},
+	groups = {oddly_breakable_by_hand = 3}, 
+});
+
+minetest.register_node("industrial_signs:chemical_warning_2", {
+	description = "Industrial Signs: Chemical Warning 2",
+	drawtype = "signlike",
+	walkable = false,
+	tiles = {"industrial_signs_chemical_warning2.png"},
+	wield_image =  "industrial_signs_chemical_warning2.png",
+	inventory_image =  "industrial_signs_chemical_warning2.png",
+	paramtype = "light",
+	paramtype2 = "wallmounted",
+	selection_box = {
+		type = "wallmounted",
+	},
+	groups = {oddly_breakable_by_hand = 3}, 
+});
+
+minetest.register_node("industrial_signs:corrosive", {
+	description = "Industrial Signs: Corrosive",
+	drawtype = "signlike",
+	walkable = false,
+	tiles = {"industrial_signs_corrosive.png"},
+	wield_image =  "industrial_signs_corrosive.png",
+	inventory_image =  "industrial_signs_corrosive.png",
+	paramtype = "light",
+	paramtype2 = "wallmounted",
+	selection_box = {
+		type = "wallmounted",
+	},
+	groups = {oddly_breakable_by_hand = 3}, 
+});
+
+minetest.register_node("industrial_signs:electric", {
+	description = "Industrial Signs: Electric",
+	drawtype = "signlike",
+	walkable = false,
+	tiles = {"industrial_signs_electric.png"},
+	wield_image =  "industrial_signs_electric.png",
+	inventory_image =  "industrial_signs_electric.png",
+	paramtype = "light",
+	paramtype2 = "wallmounted",
+	selection_box = {
+		type = "wallmounted",
+	},
+	groups = {oddly_breakable_by_hand = 3}, 
+});
+
+minetest.register_node("industrial_signs:explosive", {
+	description = "Industrial Signs: Explosive",
+	drawtype = "signlike",
+	walkable = false,
+	tiles = {"industrial_signs_explosive.png"},
+	wield_image =  "industrial_signs_explosive.png",
+	inventory_image =  "industrial_signs_explosive.png",
+	paramtype = "light",
+	paramtype2 = "wallmounted",
+	selection_box = {
+		type = "wallmounted",
+	},
+	groups = {oddly_breakable_by_hand = 3}, 
+});
+
+minetest.register_node("industrial_signs:flammable", {
+	description = "Industrial Signs: Flammable",
+	drawtype = "signlike",
+	walkable = false,
+	tiles = {"industrial_signs_flammable.png"},
+	wield_image =  "industrial_signs_flammable.png",
+	inventory_image =  "industrial_signs_flammable.png",
+	paramtype = "light",
+	paramtype2 = "wallmounted",
+	selection_box = {
+		type = "wallmounted",
+	},
+	groups = {oddly_breakable_by_hand = 3}, 
+});
+
+minetest.register_node("industrial_signs:hazard", {
+	description = "Industrial Signs: Hazard",
+	drawtype = "signlike",
+	walkable = false,
+	tiles = {"industrial_signs_hazard.png"},
+	wield_image =  "industrial_signs_hazard.png",
+	inventory_image =  "industrial_signs_hazard.png",
+	paramtype = "light",
+	paramtype2 = "wallmounted",
+	selection_box = {
+		type = "wallmounted",
+	},
+	groups = {oddly_breakable_by_hand = 3}, 
+});
+
+minetest.register_node("industrial_signs:industrial_vehicles", {
+	description = "Industrial Signs: Industrial Vehicles",
+	drawtype = "signlike",
+	walkable = false,
+	tiles = {"industrial_signs_industrial_vehicles.png"},
+	wield_image =  "industrial_signs_industrial_vehicles.png",
+	inventory_image =  "industrial_signs_industrial_vehicles.png",
+	paramtype = "light",
+	paramtype2 = "wallmounted",
+	selection_box = {
+		type = "wallmounted",
+	},
+	groups = {oddly_breakable_by_hand = 3}, 
+});
+
+minetest.register_node("industrial_signs:laser_beam", {
+	description = "Industrial Signs: Laser Beam",
+	drawtype = "signlike",
+	walkable = false,
+	tiles = {"industrial_signs_laser_beam.png"},
+	wield_image =  "industrial_signs_laser_beam.png",
+	inventory_image =  "industrial_signs_laser_beam.png",
+	paramtype = "light",
+	paramtype2 = "wallmounted",
+	selection_box = {
+		type = "wallmounted",
+	},
+	groups = {oddly_breakable_by_hand = 3}, 
+});
+
+minetest.register_node("industrial_signs:non_ionising_radiation", {
+	description = "Industrial Signs: Non Ionising Radiation",
+	drawtype = "signlike",
+	walkable = false,
+	tiles = {"industrial_signs_non_ionising_radiation.png"},
+	wield_image =  "industrial_signs_non_ionising_radiation.png",
+	inventory_image =  "industrial_signs_non_ionising_radiation.png",
+	paramtype = "light",
+	paramtype2 = "wallmounted",
+	selection_box = {
+		type = "wallmounted",
+	},
+	groups = {oddly_breakable_by_hand = 3}, 
+});
+
+minetest.register_node("industrial_signs:overhead_loads", {
+	description = "Industrial Signs: Overhead Loads",
+	drawtype = "signlike",
+	walkable = false,
+	tiles = {"industrial_signs_overhead_loads.png"},
+	wield_image =  "industrial_signs_overhead_loads.png",
+	inventory_image =  "industrial_signs_overhead_loads.png",
+	paramtype = "light",
+	paramtype2 = "wallmounted",
+	selection_box = {
+		type = "wallmounted",
+	},
+	groups = {oddly_breakable_by_hand = 3}, 
+});
+
+minetest.register_node("industrial_signs:oxidizing_material", {
+	description = "Industrial Signs: Oxidizing Material",
+	drawtype = "signlike",
+	walkable = false,
+	tiles = {"industrial_signs_oxidizing_material.png"},
+	wield_image =  "industrial_signs_oxidizing_material.png",
+	inventory_image =  "industrial_signs_oxidizing_material.png",
+	paramtype = "light",
+	paramtype2 = "wallmounted",
+	selection_box = {
+		type = "wallmounted",
+	},
+	groups = {oddly_breakable_by_hand = 3}, 
+});
+
+minetest.register_node("industrial_signs:protective_earmuffs", {
+	description = "Industrial Signs: Protective Earmuffs",
+	drawtype = "signlike",
+	walkable = false,
+	tiles = {"industrial_signs_protective_earmuffs.png"},
+	wield_image =  "industrial_signs_protective_earmuffs.png",
+	inventory_image =  "industrial_signs_protective_earmuffs.png",
+	paramtype = "light",
+	paramtype2 = "wallmounted",
+	selection_box = {
+		type = "wallmounted",
+	},
+	groups = {oddly_breakable_by_hand = 3}, 
+});
+
+minetest.register_node("industrial_signs:protective_glasses", {
+	description = "Industrial Signs: Protective Glasses",
+	drawtype = "signlike",
+	walkable = false,
+	tiles = {"industrial_signs_protective_glasses.png"},
+	wield_image =  "industrial_signs_protective_glasses.png",
+	inventory_image =  "industrial_signs_protective_glasses.png",
+	paramtype = "light",
+	paramtype2 = "wallmounted",
+	selection_box = {
+		type = "wallmounted",
+	},
+	groups = {oddly_breakable_by_hand = 3}, 
+});
+
+minetest.register_node("industrial_signs:protective_gloves", {
+	description = "Industrial Signs: Protective Gloves",
+	drawtype = "signlike",
+	walkable = false,
+	tiles = {"industrial_signs_protective_gloves.png"},
+	wield_image =  "industrial_signs_protective_gloves.png",
+	inventory_image =  "industrial_signs_protective_gloves.png",
+	paramtype = "light",
+	paramtype2 = "wallmounted",
+	selection_box = {
+		type = "wallmounted",
+	},
+	groups = {oddly_breakable_by_hand = 3}, 
+});
+
+minetest.register_node("industrial_signs:protective_helmet", {
+	description = "Industrial Signs: Protective Helmet",
+	drawtype = "signlike",
+	walkable = false,
+	tiles = {"industrial_signs_protective_helmet.png"},
+	wield_image =  "industrial_signs_protective_helmet.png",
+	inventory_image =  "industrial_signs_protective_helmet.png",
+	paramtype = "light",
+	paramtype2 = "wallmounted",
+	selection_box = {
+		type = "wallmounted",
+	},
+	groups = {oddly_breakable_by_hand = 3}, 
+});
+
+minetest.register_node("industrial_signs:protective_mask", {
+	description = "Industrial Signs: Protective Mask",
+	drawtype = "signlike",
+	walkable = false,
+	tiles = {"industrial_signs_protective_mask.png"},
+	wield_image =  "industrial_signs_protective_mask.png",
+	inventory_image =  "industrial_signs_protective_mask.png",
+	paramtype = "light",
+	paramtype2 = "wallmounted",
+	selection_box = {
+		type = "wallmounted",
+	},
+	groups = {oddly_breakable_by_hand = 3}, 
+});
+
+minetest.register_node("industrial_signs:protective_shoes", {
+	description = "Industrial Signs: Protective Shoes",
+	drawtype = "signlike",
+	walkable = false,
+	tiles = {"industrial_signs_protective_shoes.png"},
+	wield_image =  "industrial_signs_protective_shoes.png",
+	inventory_image =  "industrial_signs_protective_shoes.png",
+	paramtype = "light",
+	paramtype2 = "wallmounted",
+	selection_box = {
+		type = "wallmounted",
+	},
+	groups = {oddly_breakable_by_hand = 3}, 
+});
+
+minetest.register_node("industrial_signs:protective_suit", {
+	description = "Industrial Signs: Protective Suit",
+	drawtype = "signlike",
+	walkable = false,
+	tiles = {"industrial_signs_protective_suit.png"},
+	wield_image =  "industrial_signs_protective_suit.png",
+	inventory_image =  "industrial_signs_protective_suit.png",
+	paramtype = "light",
+	paramtype2 = "wallmounted",
+	selection_box = {
+		type = "wallmounted",
+	},
+	groups = {oddly_breakable_by_hand = 3}, 
+});
+
+minetest.register_node("industrial_signs:radioactive_material", {
+	description = "Industrial Signs: Radioactive Material",
+	drawtype = "signlike",
+	walkable = false,
+	tiles = {"industrial_signs_radioactive_material.png"},
+	wield_image =  "industrial_signs_radioactive_material.png",
+	inventory_image =  "industrial_signs_radioactive_material.png",
+	paramtype = "light",
+	paramtype2 = "wallmounted",
+	selection_box = {
+		type = "wallmounted",
+	},
+	groups = {oddly_breakable_by_hand = 3}, 
+});
+
+minetest.register_node("industrial_signs:risk_of_falling", {
+	description = "Industrial Signs: Risk Of Falling",
+	drawtype = "signlike",
+	walkable = false,
+	tiles = {"industrial_signs_risk_of_falling.png"},
+	wield_image =  "industrial_signs_risk_of_falling.png",
+	inventory_image =  "industrial_signs_risk_of_falling.png",
+	paramtype = "light",
+	paramtype2 = "wallmounted",
+	selection_box = {
+		type = "wallmounted",
+	},
+	groups = {oddly_breakable_by_hand = 3}, 
+});
+
+minetest.register_node("industrial_signs:strong_magnetic_field", {
+	description = "Industrial Signs: Strong Magnetic Field",
+	drawtype = "signlike",
+	walkable = false,
+	tiles = {"industrial_signs_strong_magnetic_field.png"},
+	wield_image =  "industrial_signs_strong_magnetic_field.png",
+	inventory_image =  "industrial_signs_strong_magnetic_field.png",
+	paramtype = "light",
+	paramtype2 = "wallmounted",
+	selection_box = {
+		type = "wallmounted",
+	},
+	groups = {oddly_breakable_by_hand = 3}, 
+});
+
+minetest.register_node("industrial_signs:toxic_material", {
+	description = "Industrial Signs: Toxic Material",
+	drawtype = "signlike",
+	walkable = false,
+	tiles = {"industrial_signs_toxic_material.png"},
+	wield_image =  "industrial_signs_toxic_material.png",
+	inventory_image =  "industrial_signs_toxic_material.png",
+	paramtype = "light",
+	paramtype2 = "wallmounted",
+	selection_box = {
+		type = "wallmounted",
+	},
+	groups = {oddly_breakable_by_hand = 3}, 
+});
+
+print("Industrial Signs v0.1 init OK. Work safe!")
